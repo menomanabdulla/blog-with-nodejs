b6a6d2912d7c28febba84add47b722470512138c
nomanabdulla <noman.fcibappy@gmail.com>
2018-10-22 03:08:17 +0600

    added UML diagram and basic userController

--- api/router/postRouter.js
+++ api/router/postRouter.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const router = express.router();
 
-router.get('/',(req,res,next)=>{
 
-})
+router.get('/',(req,res,next)=>{
+    
+});
 
 router.post('/',(req,res,next)=>{
 
-})
+});
 
 router.get('/:id',(req,res,next)=>{
 
-})
+});
 
 router.put('/:id',(req,res,next)=>{
 
-})
+});
 
 router.patch('/:id',(req,res,next)=>{
 
-})
+});
 
 router.delete('/:id',(req,res,next)=>{
 
-})
+});
 
 
 module.exports = router
--- api/router/userRouter.js
+++ api/router/userRouter.js
@@ -4,26 +4,26 @@ const userController = require();
 //user route
 router.post('/signup',(req,res,next)=>{
 
-})
+});
 
 router.post('/signin',(req,res,next)=>{
     
-})
+});
 
 router.get('/',(req,res,next)=>{
     
-})
+});
 
 router.get('/:id',(req,res,next)=>{
     
-})
+});
 
 router.put('/:id',(req,res,next)=>{
     
-})
+});
 
 router.patch('/:id',(req,res,next)=>{
     
-})
+});
 
 module.export = router;
